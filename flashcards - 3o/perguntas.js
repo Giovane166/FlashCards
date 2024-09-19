77ba6b51222f60b6939a80abebd1cbf5131c3149
Maryhlb <128267349+Maryhlb@users.noreply.github.com>
2024-09-19 08:04:03 -0300

    ftgjsrg

---
+++ flashcards - 3o/perguntas.js
@@ -0,0 +1,109 @@
+criaCartao(
+    'Química',
+    'O que são óxidos?',
+    'Compostos binários, sendo iônicos ou moleculares.')
+
+criaCartao(
+    'Química',
+    'Os óxidos ácidos em meio aquoso produzem ________ (ácidos/bases).',
+    'Ácidos')    
+
+    
+criaCartao(
+    'Química',
+    'Um óxido anfótero somado a uma base ou ácido forte dá origem a sal e água.',
+    'Verdadeiro.')    
+
+    
+criaCartao(
+    'Física',
+    'O qu se estuda em Termodinâmica?',
+    'Relação entre calor e o trabalho ocorrido em dados fenômenos.')    
+
+    
+criaCartao(
+    'Física',
+    'Caso um gás seja confinado e sofra expansão, o trabalho realizado é ________ (positivo/negativo).',
+    'Positivo.')    
+
+    
+criaCartao(
+    'Física',
+    'Caso a variação de temperatura interna (∆U) seja positiva, o gás esquentará.',
+    'Verdadeiro.')    
+
+    
+criaCartao(
+    'Língua Portuguesa',
+    'Quais são os 2 tipos de concordância?',
+    'Verbal e Nominal.')    
+
+    
+criaCartao(
+    'Língua Portuguesa',
+    'Quando ocorre concordância nominal?',
+    'Quando ocorre a flexão de um termo para concordar com um substantivo.')    
+
+    
+criaCartao(
+    'Língua Portuguesa',
+    'Quando ocorre concordância verbal?',
+    'Quando se dá a flexão de um verbo para concordar com o sujeito.')    
+
+criaCartao(
+    'Biologia',
+    'O Fixismo também é conhecido como?',
+    'Criacionismo')    
+
+criaCartao(
+    '',
+    '',
+    ' ')    
+
+criaCartao(
+'',
+'',
+' ')    
+
+criaCartao(
+'',
+'',
+' ')    
+criaCartao(
+'',
+'',
+' ')    
+
+criaCartao(
+'',
+'',
+' ')    
+
+criaCartao(
+'',
+'',
+' ')    
+
+criaCartao(
+'',
+'',
+' ')    
+criaCartao(
+'',
+'',
+' ')    
+criaCartao(
+'',
+'',
+' ')    
+
+criaCartao(
+'',
+'',
+' ')    
+
+criaCartao(
+'',
+'',
+' ')    
+                                                                                                                                                                                                                            
